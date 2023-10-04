9864e93f034ed2bed2c3707a2cb00f36b5b1370d
aadityadike <adityadike123@gmail.com>
2023-10-04 10:10:55 +0530

    Let's learn about Inheritance

--- src/com/aditya/Inheritance/Iphone.java
+++ src/com/aditya/Inheritance/Iphone.java
@@ -7,8 +7,20 @@ public class Iphone extends SmartPhone {
         this.browser = "Safari";
     }
 
+    public Iphone(String browser) {
+        this.browser = browser;
+    }
+
     public Iphone(String os, String camera, int battery, boolean cableType, String browser) {
         super(os, camera, battery, cableType);
         this.browser = browser;
+    }
+
+    public Iphone(Iphone phone) {
+        this.os = phone.os;
+        this.battery = phone.battery;
+        this.camera = phone.camera;
+        this.browser = phone.browser;
+        this.cableType = phone.cableType;
     }
 }
--- src/com/aditya/Inheritance/Main.java
+++ src/com/aditya/Inheritance/Main.java
@@ -2,7 +2,9 @@ package com.aditya.Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Iphone phone1 = new Iphone();
+        Iphone phone1 = new Iphone("chromium");
+        Iphone phone2 = new Iphone(phone1);
         System.out.println(phone1.browser);
+        System.out.println(phone2.os);
     }
 }
